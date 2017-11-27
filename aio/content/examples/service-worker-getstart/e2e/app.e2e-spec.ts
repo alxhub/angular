e0d7fe36b222063f3fa5d63fdbb4d7ec85ea5a48
Kapunahele Wong <kapunahelewong@gmail.com>
2017-11-27 13:48:32 -0500

    docs(aio): add service worker guide content and update nav

---
+++ aio/content/examples/service-worker-getstart/e2e/app.e2e-spec.ts
@@ -0,0 +1,21 @@
+import { AppPage } from './app.po';
+import { browser, element, by } from 'protractor';
+
+describe('sw-example App', () => {
+  let page: AppPage;
+  let logo = element(by.css("img"));
+
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to Service Workers!');
+  });
+
+  it('should display the Angular logo', () => {
+    page.navigateTo();
+    expect(logo.isPresent()).toBe(true); });
+});
