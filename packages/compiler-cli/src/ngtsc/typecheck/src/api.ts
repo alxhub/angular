27969cbe2adb846c76abdeaf1dca25302a097394
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-26 09:43:52 -0700

    feat(ivy): introduce typecheck package and a type constructor generator
    This commit introduces //packages/compiler-cli/src/ngtsc/typecheck as a
    container for template type-checking code, and implements an initial API:
    type constructor generation.
    
    Type constructors are static methods on component/directive types with
    no runtime implementation. The methods are used during compilation to
    enable inference of a component or directive's generic type parameters
    from the types of expressions bound to any of their @Inputs. A type
    constructor looks like:
    
    class Directive<T> {
      someInput: T;
      static ngTypeCtor<T>(init: Partial<Pick<Directive<T>, 'someInput'>>): Directive<T>;
    }
    
    It can be used to infer a type for T based on the input:
    
    const _dir = Directive.ngTypeCtor({someInput: 'string'}); // Directive<T>

---
+++ packages/compiler-cli/src/ngtsc/typecheck/src/api.ts
@@ -0,0 +1,27 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import * as ts from 'typescript';
+import {Directive, Node} from '@angular/compiler';
+
+import {Reference} from '../../metadata';
+
+export interface DirectiveTypecheckData { ref: Reference<ts.ClassDeclaration>; }
+
+export interface ComponentTcbRequest {
+  nodes: Node[];
+  directives: Directive<DirectiveTypecheckData>;
+}
+
+export interface ComponentTcbResponse { checkFn: ts.FunctionDeclaration; }
+
+export interface TypeCtorMetadata {
+  fnName: string;
+  body: boolean;
+  fields: {inputs: string[]; outputs: string[]; queries: string[];};
+}
---
+++ packages/compiler-cli/src/ngtsc/typecheck/src/type_constructor.ts
@@ -0,0 +1,63 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import * as ts from 'typescript';
+
+import {TypeCtorMetadata} from './api';
+
+export function generateTypeCtor(
+    node: ts.ClassDeclaration, meta: TypeCtorMetadata): ts.MethodDeclaration {
+  const id = node.name !;
+  const rawTypeArgs = node.typeParameters !== undefined ?
+      node.typeParameters.map(param => ts.createTypeReferenceNode(param.name, undefined)) :
+      undefined;
+
+  const rawType: ts.TypeNode = ts.createTypeReferenceNode(id, rawTypeArgs);
+
+  let initType: ts.TypeNode;
+
+  const keys: string[] = [
+    ...meta.fields.inputs,
+    ...meta.fields.outputs,
+    ...meta.fields.queries,
+  ];
+  if (keys.length === 0) {
+    initType = ts.createTypeLiteralNode([]);
+  } else {
+    const keyTypeUnion = ts.createUnionTypeNode(
+        keys.map(key => ts.createLiteralTypeNode(ts.createStringLiteral(key))));
+
+    const pickType = ts.createTypeReferenceNode('Pick', [rawType, keyTypeUnion]);
+    initType = ts.createTypeReferenceNode('Partial', [pickType]);
+  }
+
+  const body = meta.body ? ts.createBlock([
+    ts.createReturn(ts.createNonNullExpression(ts.createNull())),
+  ]) :
+                           undefined;
+
+  const initParam = ts.createParameter(
+      /* decorators */ undefined,
+      /* modifiers */ undefined,
+      /* dotDotDotToken */ undefined,
+      /* name */ 'init',
+      /* questionToken */ undefined,
+      /* type */ initType,
+      /* initializer */ undefined, );
+
+  return ts.createMethod(
+      /* decorators */ undefined,
+      /* modifiers */[ts.createModifier(ts.SyntaxKind.StaticKeyword)],
+      /* asteriskToken */ undefined,
+      /* name */ meta.fnName,
+      /* questionToken */ undefined,
+      /* typeParameters */ node.typeParameters,
+      /* parameters */[initParam],
+      /* type */ rawType,
+      /* body */ body, );
+}
