01b376389d525fb055366f2718cafa50c6d703b0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 14:37:32 -0800

    refactor(ivy): move the flat module index generator to its own package
    This commit moves the FlatIndexGenerator to its own package, in preparation
    to expand its capabilities and support re-exporting of private declarations
    from NgModules.

---
+++ packages/compiler-cli/src/ngtsc/util/src/typescript.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import * as ts from 'typescript';
+
+const D_TS = /\.d\.ts$/i;
+
+export function isDtsPath(filePath: string): boolean {
+  return D_TS.test(filePath);
+}
