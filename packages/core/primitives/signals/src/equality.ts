cd52261a6d087dd01b192a7107491faadf9f7505
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-06 11:04:49 -0700

    g3patch: mutate

--- packages/core/primitives/signals/src/equality.ts
+++ packages/core/primitives/signals/src/equality.ts
@@ -15,5 +15,5 @@ export type ValueEqualityFn<T> = (a: T, b: T) => boolean;
  * The default equality function used for `signal` and `computed`, which uses referential equality.
  */
 export function defaultEquals<T>(a: T, b: T) {
-  return Object.is(a, b);
+  return (a === null || typeof a !== 'object') && Object.is(a, b);
 }
