e9092a7bff0e0b690803f0cac2060fb098756361
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-08-01 13:45:23 -0700

    refactor(core): implement `nestedSignal` and `structuralSignal`
    These are new signal variants that optimize the way updates are written in
    large chains of `nestedSignal`s. This optimization primarily avoids
    invalidating `nestedSignal`s that aren't affected by writes, even if they
    derive from parents whose values are changed. See the added `README.md` for
    details.

--- packages/core/src/core_reactivity_export_internal.ts
+++ packages/core/src/core_reactivity_export_internal.ts
@@ -11,6 +11,14 @@ export {SIGNAL as ɵSIGNAL} from '../primitives/signals';
 export {isSignal, Signal, ValueEqualityFn} from './render3/reactivity/api';
 export {computed, CreateComputedOptions} from './render3/reactivity/computed';
 export {
+  nestedSignal as ɵnestedSignal,
+  NestedSignalOptions as ɵNestedSignalOptions,
+} from './render3/reactivity/nested_signal/nested_signal';
+export {
+  structuralSignal as ɵstructuralSignal,
+  StructuralSignalOptions as ɵStructuralSignalOptions,
+} from './render3/reactivity/nested_signal/structural_signal';
+export {
   CreateSignalOptions,
   signal,
   WritableSignal,
